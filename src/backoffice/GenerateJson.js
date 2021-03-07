b2f01fe2339ad3df74c1c072cbe5f7b9fe183f14
unknown <mick.y@hotmail.com>
2021-03-07 14:10:19 +0700

    asdasd

--- src/backoffice/GenerateJson.js
+++ src/backoffice/GenerateJson.js
@@ -7,10 +7,16 @@ import './css/ActivityBack.css';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import CreateAdmin from './CreateAdmin';
-const HOST_URL_IMG = "https://give-child.aowdtech.com/resources/uploads/";
+const HOST_URL_IMG = "https://give-for-child.thesis-su2020ict.com/resources/uploads/";
+
+const LimitString = (msg, count) => {
+    return msg.substring(0, count) + (msg.length > count ? '...' : '')
+}
+
 const GenerateJson = () => {
     const [rowData, setRowData] = useState([]);
     const [dataJson, setDataJson] = useState('');
+
 
     useEffect(() => {
         getFoundation();
@@ -24,14 +30,37 @@ const GenerateJson = () => {
                     if (res.data) {
                         setRowData(res.data);
                         const foundationObj = res.data.map(f => {
+
+                            return {
+                                    actions: [
+                                      {
+                                        label: "รายละเอียด",
+                                        uri: f.url,
+                                        type: "uri"
+                                      }
+                                    ],
+                                    title: LimitString(f.name,30),
+                                    defaultAction: {
+                                        label: "รายละเอียด",
+                                        uri: f.url,
+                                      type: "uri"
+                                    },
+                                    imageBackgroundColor: "#FFFFFF",
+                                    text: LimitString(f.address,40),
+                                    thumbnailImageUrl: HOST_URL_IMG + f.img
+                                  
+                            }
+
+
+
                             return{
                                 defaultAction: {
                                     label: "รายละเอียด",
                                     uri: f.url,
                                     type: "uri"
                                 },
-                                text: f.address,
-                                title: f.name,
+                                text: LimitString(f.address,40),
+                                title: LimitString(f.name,20),
                                 actions: [
                                   {
                                     type: "uri",
